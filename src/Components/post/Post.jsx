a48ae5631a0a8d0bd0671578a5c9365b17724cd1
tamim-18 <tamim11903060@gmail.com>
2023-10-30 12:54:25 +0600

    css added

--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { MoreVert } from "@mui/icons-material";
 
 function Post() {
-  return <div>Hello mf</div>;
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              src="/assets/person/1.jpeg"
+              alt=""
+              className="postProfileImg"
+            />
+            <span className="name">Taohid Khan Tamim</span>
+            <span className="time">5 minutes ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter"></div>
+        <div className="postBottom"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Post;
